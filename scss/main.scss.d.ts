444d4a770519854cead59dc59c4c3649abc87627
Horlamijazzy <roderrick007@gmail.com>
2022-02-25 21:05:24 +0100

    initial

---
+++ scss/main.scss.d.ts
@@ -0,0 +1,9 @@
+import { StyleProp, TextStyle } from 'react-native'
+
+export const container: StyleProp<TextStyle>
+export const ajax-section: StyleProp<TextStyle>
+export const cities: StyleProp<TextStyle>
+export const city: StyleProp<TextStyle>
+export const city::after: StyleProp<TextStyle>
+export const city-temp: StyleProp<TextStyle>
+export const city-icon: StyleProp<TextStyle>
